17e9168c2e4751ec53f4e049806023db49ae92ec
Alked <ae7g18@soton.ac.uk>
2020-05-17 21:18:52 +0200

    Modify script for macOS

--- src/script.sh
+++ src/script.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-gnome-terminal --geometry=100x30 -- /bin/sh -c 'javac Coordinator.java; java Coordinator 1243 4' &
-sleep 5
-gnome-terminal --geometry=80x10 -- /bin/sh -c 'javac Participant.java; java Participant 1243 2222' &
+osascript -e 'tell app "Terminal" to do script "cd /Users/alked/IdeaProjects/consensus-protocol/src; javac Coordinator.java; java Coordinator 1243 4"'
 sleep 3
-gnome-terminal --geometry=80x10 -- /bin/sh -c 'javac Participant.java; java Participant 1243 3333' &
+
+osascript -e 'tell app "Terminal" to do script "cd /Users/alked/IdeaProjects/consensus-protocol/src; javac Participant.java; java Participant 1243 2222"'
 sleep 3
-gnome-terminal --geometry=80x10 -- /bin/sh -c 'javac Participant.java; java Participant 1243 4444' &
+
+osascript -e 'tell app "Terminal" to do script "cd /Users/alked/IdeaProjects/consensus-protocol/src; javac Participant.java; java Participant 1243 4444"'
 sleep 3
-gnome-terminal --geometry=80x10 -- /bin/sh -c 'javac Participant.java; java Participant 1243 5555'
+
+osascript -e 'tell app "Terminal" to do script "cd /Users/alked/IdeaProjects/consensus-protocol/src; javac Participant.java; java Participant 1243 3333"'
+sleep 3
+
+osascript -e 'tell app "Terminal" to do script "cd /Users/alked/IdeaProjects/consensus-protocol/src; javac Participant.java; java Participant 1243 5555"'
 
 rm *.class
